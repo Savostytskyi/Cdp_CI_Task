1ba8d473caba4bbdd7abd9af695ca12be0e75492
savostytskyi <savostytskyi@epam.com>
2015-10-14 19:31:45 +0300

    version 1.0.3

--- cdp/src/main/java/com/epam/ci/Main.java
+++ cdp/src/main/java/com/epam/ci/Main.java
@@ -1,6 +1,8 @@
 package com.epam.ci;
 
 import com.epam.ci.download.Constants;
+import org.apache.log4j.Logger;
+
 import java.io.File;
 import static com.epam.ci.download.IOHelper.downloadFileFromURL;
 
@@ -9,9 +11,12 @@ import static com.epam.ci.download.IOHelper.downloadFileFromURL;
  */
 public class Main {
 
+    private static final Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
 
         File file = new File(Constants.PATH_TO_DOWNLOADED_FILE);
+        logger.info("Create file for received data.");
         try {
             downloadFileFromURL(Constants.DOWNLOAD_FILE_URL, file);
         } catch (Exception ex) {
--- cdp/src/main/java/com/epam/ci/download/IOHelper.java
+++ cdp/src/main/java/com/epam/ci/download/IOHelper.java
@@ -6,18 +6,25 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author Savostytskyi Anton on 14.10.2015.
  */
 public class IOHelper {
 
+    private static final Logger logger = Logger.getLogger(IOHelper.class);
+
     public static void downloadFileFromURL(String urlString, File destination) throws Exception {
         URL website = new URL(urlString);
-        try(
+        logger.info("Connect to url and try to read file.");
+        try (
                 ReadableByteChannel rbc = Channels.newChannel(website.openStream());
                 FileOutputStream fos = new FileOutputStream(destination)
-        ){
+        ) {
+            logger.info("Get data from file.");
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            logger.info("Write data to file.");
         }
     }
 }
