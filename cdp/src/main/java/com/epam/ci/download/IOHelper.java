090f13b657142415f99fa9298699eebe1971c7c5
savostytskyi <savostytskyi@epam.com>
2015-10-14 14:46:54 +0300

    minor changes

--- cdp/src/main/java/com/epam/ci/download/IOHelper.java
+++ cdp/src/main/java/com/epam/ci/download/IOHelper.java
@@ -1,7 +1,23 @@
 package com.epam.ci.download;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
 /**
  * @author Savostytskyi Anton on 14.10.2015.
  */
 public class IOHelper {
+
+    public static void downloadFileFromURL(String urlString, File destination) throws Throwable {
+        URL website = new URL(urlString);
+        try(
+                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream(destination)
+        ){
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+    }
 }
